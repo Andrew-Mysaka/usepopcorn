08b172606cdc61455fa49acb017aba0cec0c547b
Andrew-Mysaka <andrewmysaka@gmail.com>
2023-10-24 19:51:08 +0300

    passing elements as props (alternative to children

--- src/App.js
+++ src/App.js
@@ -64,6 +64,16 @@ export default function App() {
             </NavBar>
 
             <Main>
+                {/*<Box element={*/}
+                {/*    <MovieList movies={movies}/>*/}
+                {/*}/>*/}
+                {/*<Box element={*/}
+                {/*    <>*/}
+                {/*        <WatchedSummary watched={watched}/>*/}
+                {/*        <WatchedMoviesList watched={watched}/>*/}
+                {/*    </>*/}
+                {/*}/>*/}
+
                 <Box>
                     <MovieList movies={movies}/>
                 </Box>
