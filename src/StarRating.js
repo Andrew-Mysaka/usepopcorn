fda1383512ab532a154c0690d8f28e78cbed17ec
Andrew-Mysaka <andrewmysaka@gmail.com>
2023-10-24 21:10:24 +0300

    handling hover events

--- src/StarRating.js
+++ src/StarRating.js
@@ -16,7 +16,8 @@ const textStyle = {
 }
 
 export default function StarRating({maxRating = 5}) {
-    const [rating, setRating] = useState(1);
+    const [rating, setRating] = useState(0);
+    const [tempRating, setTempRating] = useState(0);
 
     function handleRating(rating) {
         setRating(rating);
@@ -26,10 +27,16 @@ export default function StarRating({maxRating = 5}) {
         <div style={containerStyle}>
             <div style={starContainerStyle}>
                 {Array.from({length:maxRating}, (_,i) =>
-                    <Star key={i} onRate={() => handleRating(i+1)} full={rating >= i + 1}/>
+                    <Star
+                        key={i}
+                        onRate={() => handleRating(i+1)}
+                        full={tempRating ? tempRating >= i + 1 : rating >= i + 1}
+                        onHoverIn={() => setTempRating(i + 1)}
+                        onHoverOut={() => setTempRating(0)}
+                    />
                 )}
             </div>
-            <p style={textStyle}>{rating || ""}</p>
+            <p style={textStyle}>{tempRating || rating || ""}</p>
         </div>
     );
 }
@@ -41,9 +48,14 @@ const starStyles = {
     cursor: 'pointer',
 }
 
-function Star({onRate, full}) {
+function Star({onRate, full, onHoverIn, onHoverOut}) {
     return (
-        <span role={'button'} style={starStyles} onClick={onRate}>
+        <span role={'button'}
+              style={starStyles}
+              onClick={onRate}
+              onMouseEnter={onHoverIn}
+              onMouseLeave={onHoverOut}
+        >
             {full ?
             <svg
                 xmlns="http://www.w3.org/2000/svg"
