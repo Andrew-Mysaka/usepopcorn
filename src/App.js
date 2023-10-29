2421d7e467f08a74ad21d1b86926998ee00efd7f
Andrew-Mysaka <andrewmysaka@gmail.com>
2023-10-29 19:45:32 +0200

    lesson 156 - Cleaning Up data fetching

--- src/App.js
+++ src/App.js
@@ -32,11 +32,14 @@ export default function App() {
     }
 
     useEffect(function () {
+        const controller = new AbortController();
+
         async function fetchMovies() {
             try {
                 setIsLoading(true);
                 setError('');
-                const res = await fetch(`http://www.omdbapi.com/?apikey=${KEY}&s=${query}`);
+                const res = await fetch(`http://www.omdbapi.com/?apikey=${KEY}&s=${query}`,
+                    {signal: controller.signal});
 
                 if (!res.ok) throw new Error("Something went wrong with fetching movies!");
 
@@ -45,9 +48,13 @@ export default function App() {
                 if (data.Response === 'False') throw new Error("Movie not found!");
 
                 setMovies(data.Search);
+                setError('');
             } catch (err) {
                 console.log(err.message);
-                setError(err.message);
+
+                if(err.name !== 'AbortError'){
+                    setError(err.message);
+                }
             } finally {
                 setIsLoading(false);
             }
@@ -60,6 +67,10 @@ export default function App() {
         }
 
         fetchMovies();
+
+        return function () {
+            controller.abort();
+        };
     }, [query]);
 
     return (
