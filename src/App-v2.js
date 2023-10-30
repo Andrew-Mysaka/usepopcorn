546df1a545c36476d5b5237cd7c10eeeb5be1f9e
Andrew-Mysaka <andrewmysaka@gmail.com>
2023-10-30 20:23:05 +0200

    lesson 163 - initializing state with a callback (lazy initial state)

--- src/App-v2.js
+++ src/App-v2.js
@@ -10,13 +10,10 @@ const KEY = '979514d0';
 export default function App() {
     const [query, setQuery] = useState("");
     const [movies, setMovies] = useState([]);
-    // const [watched, setWatched] = useState([]);
+    const [watched, setWatched] = useState([]);
     const [isLoading, setIsLoading] = useState(false);
     const [error, setError] = useState('');
     const [selectedId, setSelectedId] = useState(null);
-    const [watched, setWatched] = useState(function (){
-        return JSON.parse(localStorage.getItem("watched"));
-    });
 
     function handleSelectMovie(id){
         setSelectedId(selectedId => id === selectedId ? null : id);
@@ -27,17 +24,12 @@ export default function App() {
     }
 
     function handleAddWatched(movie) {
-        setWatched(watched => [...watched, movie]);
-        // localStorage.setItem("watched", JSON.stringify([...watched, movie]));
+        setWatched(watched => [...watched, movie])
     }
 
     function handleDeleteWatched(id) {
         setWatched((watched) => watched.filter(movie => movie.imdbID !== id));
     }
-
-    useEffect(function (){
-        localStorage.setItem("watched", JSON.stringify(watched));
-    }, [watched])
 
     useEffect(function () {
         const controller = new AbortController();
