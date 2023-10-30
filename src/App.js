2b524b830e97e96e8d592a9ccbb3380bdedb13b3
Andrew-Mysaka <andrewmysaka@gmail.com>
2023-10-30 21:13:32 +0200

    lesson 167 - refs to select DOM elements

--- src/App.js
+++ src/App.js
@@ -345,7 +345,7 @@ function Search({query, setQuery}) {
         document.addEventListener("keydown", callback);
 
         return () => document.addEventListener("keydown", callback);
-    }, [])
+    }, [setQuery])
 
     return (
         <input
