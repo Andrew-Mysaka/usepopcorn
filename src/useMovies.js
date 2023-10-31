55b78bd0c40ab27dd1057b2b89e5727542880495
Andrew-Mysaka <andrewmysaka@gmail.com>
2023-10-31 22:23:27 +0200

    use secure page URL

--- src/useMovies.js
+++ src/useMovies.js
@@ -15,7 +15,7 @@ export function useMovies(query){
             try {
                 setIsLoading(true);
                 setError('');
-                const res = await fetch(`http://www.omdbapi.com/?apikey=${KEY}&s=${query}`,
+                const res = await fetch(`https://www.omdbapi.com/?apikey=${KEY}&s=${query}`,
                     {signal: controller.signal});
 
                 if (!res.ok) throw new Error("Something went wrong with fetching movies!");
